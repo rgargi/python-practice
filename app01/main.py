e1fc18f76c9d93f5b6e95fe1ddaed0bcfb0f857e
gargi <gargi.rudole@gmail.com>
2024-07-07 03:41:24 +0530

    add edit function

--- app01/main.py
+++ app01/main.py
@@ -1,7 +1,7 @@
 status = True
 tasks = []
 
-print("Type add, show, help, exit.")
+print("Type add(a), show, edit, help, exit.")
 
 def addtask():
     task = input("Enter a task: ")
@@ -15,19 +15,37 @@ def showtasks():
     for t in tasks:
         print("- ", t)
 
+def edittask():
+    print("Which task to edit?")
+    for i,task in enumerate(tasks):
+        print(i+1, "-", task)
+    task_no = int(input("Enter the serial number of the task: "))
+    if task_no > len(tasks) or task_no <1:
+        print("ERROR: Task with this serial number doesn't exist.")
+    else:
+        old_task = tasks[task_no-1]
+        print("Selected Task: ", old_task)
+        new_task = input("Enter updated task: ")
+        # tasks.remove(old_task)
+        # tasks.insert(task_no-1, new_task)
+        tasks[task_no-1] = new_task
+        print("Task updated!")
+
 while status:
     user_action = input("Enter a command: ")
     match user_action.strip().lower():
-        case "add":
+        case "add" | "a":
             addtask()
         case "show":
             showtasks()
+        case "edit":
+            edittask()
         case "exit":
             print("Exiting...")
             status=False
         case "help":
-            print("Type:\n'add' to add a task.\n'show' to get a list of entered tasks.\n'exit' to quit.")
+            print("Type:\n'add' to add a task.\n'show' to get a list of entered tasks.\n'edit' to edit the task\n'exit' to quit.")
         case _:
-            print("Couldn't understand.")
+            print("ERROR: Couldn't understand the command.")
 
 print("Have a good day!")
